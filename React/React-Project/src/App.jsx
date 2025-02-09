d45c9bdbe49d20e70d6f0b58e639d1099f8b6422
Paramesh-T-S <42896229+Paramesh-T-S@users.noreply.github.com>
2025-02-09 14:35:56 +0530

    Fixed all components rerender issue.
    Used separate state within the component so that unnecessary rerenders avoided. (![Image](https://github.com/user-attachments/assets/14b6e011-887b-466e-953c-6f329811f4ad))

--- React/React-Project/src/App.jsx
+++ React/React-Project/src/App.jsx
@@ -2,18 +2,26 @@ import { useState } from "react"
 
 
 function App() {
+  return (
+    <> 
+      <HeaderWithButtonComponent></HeaderWithButtonComponent>
+      <HeaderComonent headerTitle={"Paramesh Header component2"}></HeaderComonent> 
+      <HeaderComonent headerTitle={"Paramesh Header component3"}></HeaderComonent> 
+    </>
+  )
+}
+
+function HeaderWithButtonComponent(){
   const [headerTitle, setHeaderTitle] = useState("paramesh1");
 
   function updateTitle(){
     setHeaderTitle("Paramesh" + Math.random());
   }
 
-  return (
-    <> 
-      <button onClick={updateTitle}>Click here to change the title</button>
-      <HeaderComonent headerTitle={headerTitle}></HeaderComonent> 
-      <HeaderComonent headerTitle={"Paramesh Header component2"}></HeaderComonent> 
-      <HeaderComonent headerTitle={"Paramesh Header component3"}></HeaderComonent> 
+  return(
+    <>
+    <button onClick={updateTitle}>Click here to change the title</button>
+    <HeaderComonent headerTitle={headerTitle}></HeaderComonent> 
     </>
   )
 }
