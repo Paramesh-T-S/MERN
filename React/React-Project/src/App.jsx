e8c310786e2b31e78b0e835a219b2d327cf530bd
Paramesh-T-S <42896229+Paramesh-T-S@users.noreply.github.com>
2025-02-09 16:50:49 +0530

    [ToDo Application]
    Basic to do application in React - FrontEnd using rerender concept.
    
    ![Image](https://github.com/user-attachments/assets/598e13a3-505f-43e2-b0b8-9d303563e3c2)
    
    ![Image](https://github.com/user-attachments/assets/ec4ab9e0-5985-49ef-b49e-ab656dde0bd9)

--- React/React-Project/src/App.jsx
+++ React/React-Project/src/App.jsx
@@ -2,34 +2,57 @@ import React from "react";
 
 import { useState } from "react"
 
+let counter = 4; //3 items already predefied, so starting from 4
 
 function App() {
-  const [headerTitle, setHeaderTitle] = useState("paramesh1");
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: "Task1",
+      description: "Description for Task1"
+    },
+    {
+      id: 2,
+      title: "Task2",
+      description: "Description for Task2"
+    },
+    {
+      id: 3,
+      title: "Task3",
+      description: "Description for Task3"
+    }
+  ]);
 
-  function updateTitle(){
-    setHeaderTitle("Paramesh" + Math.random());
+  function updateTos(){
+    let counterLocal = counter++;
+    //using spread operator(...) to spread existing todos and appending the next one
+    setTodos([...todos,
+      {
+        id: counterLocal,
+        title: "Task : " + counterLocal,
+        description: "Description for Task : " + counterLocal
+      }
+    ])
   }
 
   return (
     <div> 
-      <button onClick={updateTitle}>Click here to change the title</button>
-      <HeaderComonent headerTitle={headerTitle}></HeaderComonent> 
-      <HeaderComonent headerTitle={"Paramesh Header component2"}></HeaderComonent> 
-      <HeaderComonent headerTitle={"Paramesh Header component3"}></HeaderComonent> 
-      <HeaderComonent headerTitle={"Paramesh Header component3"}></HeaderComonent> 
-      <HeaderComonent headerTitle={"Paramesh Header component3"}></HeaderComonent> 
-      <HeaderComonent headerTitle={"Paramesh Header component3"}></HeaderComonent> 
-      <HeaderComonent headerTitle={"Paramesh Header component3"}></HeaderComonent> 
-
+      <button onClick={updateTos}> Add ToDo button</button>
+      {/* iterating over the todos list */}
+      {todos.map((todo) => 
+        <ToDo title={todo.title} description={todo.description}></ToDo> 
+      )}
     </div>
   )
 }
 
-//Header component
-const HeaderComonent = React.memo(function HeaderComonent({headerTitle}){
+function ToDo({title, description}){
   return(
-    <div>{headerTitle}</div>
+    <div>
+      <h1>{title}</h1>
+      <h6>{description}</h6>
+    </div>
   )
-})
+}
 
 export default App
