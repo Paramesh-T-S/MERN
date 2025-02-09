7187e529d845d52e2d7cfeb312e7e07f6fafb36e
Paramesh-T-S <42896229+Paramesh-T-S@users.noreply.github.com>
2025-02-09 12:28:59 +0530

    Basic React Header component

--- React/React-Project/src/App.jsx
+++ React/React-Project/src/App.jsx
@@ -1,8 +1,17 @@
 function App() {
 
   return (
-    <>
+    <> 
+      <HeaderComonent headerTitle={"Paramesh Header component1"}></HeaderComonent> 
+      <HeaderComonent headerTitle={"Paramesh Header component2"}></HeaderComonent> 
+      <HeaderComonent headerTitle={"Paramesh Header component3"}></HeaderComonent> 
     </>
+  )
+}
+
+function HeaderComonent({headerTitle}){
+  return(
+    <div>{headerTitle}</div>
   )
 }
 
