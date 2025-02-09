c7e7a439a6e7b699424eee744d376a612347bc8d
Paramesh-T-S <42896229+Paramesh-T-S@users.noreply.github.com>
2025-02-09 14:33:43 +0530

    Button with rerender title
    Fix : Introduced rerendering title
    Problem : But it rerenders all associated components unnecessarily. (![Image](https://github.com/user-attachments/assets/a5b19b9c-0cc1-453d-a36a-59c1061ac698))

--- React/React-Project/src/App.jsx
+++ React/React-Project/src/App.jsx
@@ -1,8 +1,17 @@
+import { useState } from "react"
+
+
 function App() {
+  const [headerTitle, setHeaderTitle] = useState("paramesh1");
+
+  function updateTitle(){
+    setHeaderTitle("Paramesh" + Math.random());
+  }
 
   return (
     <> 
-      <HeaderComonent headerTitle={"Paramesh Header component1"}></HeaderComonent> 
+      <button onClick={updateTitle}>Click here to change the title</button>
+      <HeaderComonent headerTitle={headerTitle}></HeaderComonent> 
       <HeaderComonent headerTitle={"Paramesh Header component2"}></HeaderComonent> 
       <HeaderComonent headerTitle={"Paramesh Header component3"}></HeaderComonent> 
     </>
