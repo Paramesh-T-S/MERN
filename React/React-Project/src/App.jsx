5a4511528d0b83f71486251e61931da0e81518b3
Paramesh-T-S <42896229+Paramesh-T-S@users.noreply.github.com>
2025-02-09 18:35:38 +0530

    [Wrapper Components] - method 1
    Added a component inside a component

--- React/React-Project/src/App.jsx
+++ React/React-Project/src/App.jsx
@@ -2,7 +2,23 @@ function App() {
 
   return (
     <div> 
+      <CardWrapperComponent innerComponent={<TextComponent></TextComponent>} ></CardWrapperComponent>
+    </div>
+  )
+}
 
+function TextComponent(){
+  return (
+    <div>
+      Hi Paramesh2
+    </div>
+  )
+}
+
+function CardWrapperComponent({innerComponent}){
+  return(
+    <div style={{border:"2px solid"}}>
+      {innerComponent}
     </div>
   )
 }
